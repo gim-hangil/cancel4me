4453d267aee3f9c4636a2303b50a31b7732ca78f
GIM HANGIL <gim_hangil@kakao.com>
2022-11-29 12:46:31 +0000

    Add tabs
    Tabs enables user to easily switch between booking mode and current
    queue mode.

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,10 @@
-import { Box, Divider, Heading } from 'dracula-ui';
+import { Box, Heading, Tabs, Text } from 'dracula-ui';
+import { useState } from 'react';
 import SelectStation from './SelectStation';
 import './App.css';
 
 function App() {
+  const [tab, setTab] = useState('book');
   return (
     <Box className="App" width="full" mx="auto" my="xs">
       <Box className="heading" my="sm">
@@ -10,11 +12,30 @@ function App() {
           취소표가 필요해!
         </Heading>
       </Box>
+      <Tabs className="tabs">
+        <li
+          className={[
+            'drac-tab',
+            tab === 'book' ? 'drac-tab-active' : ''
+          ].join(' ')}
+          onClick={() => setTab('book')}
+        >
+          <Text>예매</Text>
+        </li>
+        <li
+          className={[
+            'drac-tab',
+            tab === 'list' ? 'drac-tab-active' : ''
+          ].join(' ')}
+          onClick={() => setTab('list')}
+        >
+          <Text>확인</Text>
+        </li>
+      </Tabs>
       <Box className="station" display="flex">
         <SelectStation label="출발" />
         <SelectStation label="도착" />
       </Box>
-      <Divider color="purple" mx="xs" my="sm" />
     </Box>
   );
 }
