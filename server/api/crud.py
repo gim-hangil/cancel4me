50a4e395db2b66a3eb49b57cf28404bbcacf805e
mayechan3848 <mega3848@gmail.com>
2022-12-02 12:31:42 +0000

    Add CRUD functions

---
+++ server/api/crud.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+
+from . import model, schema
+
+
+def create_ticket(db: Session, ticket: schema.TicketCreate):
+  db_ticket = model.Ticket(
+    korail_id=ticket.korail_id,
+    korail_pw=ticket.korail_pw,
+    departure_station=ticket.departure_station,
+    arrival_station=ticket.arrival_station,
+    date=ticket.date,
+    departure_base=ticket.departure_base,
+    arrival_limit=ticket.arrival_limit,
+  )
+  db.add(db_ticket)
+  db.commit()
+  db.refresh(db_ticket)
+  return db_ticket
+
+
+def get_tickets(db: Session, skip: int = 0, limit: int = 100):
+  return db.query(model.Ticket).offset(skip).limit(limit).all()
