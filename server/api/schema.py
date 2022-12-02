5b54eefe0933d019805aa67801739bc43498579e
mayechan3848 <mega3848@gmail.com>
2022-12-02 12:31:20 +0000

    Add request and response schema

---
+++ server/api/schema.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class TicketBase(BaseModel):
+  departure_station: str
+  arrival_station: str
+  date: str
+  departure_base: str
+  arrival_limit: str
+
+
+class TicketCreate(TicketBase):
+  korail_id: str
+  korail_pw: str
+
+
+class Ticket(TicketBase):
+  id: int
+  reserved: bool
