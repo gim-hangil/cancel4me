ad511e9af4586255a159fd483fba7d57755ea879
mayechan3848 <mega3848@gmail.com>
2022-12-02 12:32:33 +0000

    Implement ticket reservation API

--- server/api/main.py
+++ server/api/main.py
@@ -3,10 +3,25 @@
 :copyright: (c) 2022 by Hangil Gim.
 :license: MIT, see LICENSE for more details.
 """
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from . import crud, model, schema
+from .database import SessionLocal, engine
+
+
+model.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
 
 
 @app.get("/")
@@ -14,3 +29,8 @@ async def test():
   return {
     "message": "Hello World",
   }
+
+
+@app.post("/tickets", response_model=schema.Ticket)
+def create_ticket(ticket: schema.TicketCreate, db: Session=Depends(get_db)):
+  return crud.create_ticket(db=db, ticket=ticket)
