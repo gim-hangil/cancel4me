112f2ed7e90b95428133c65e59373b1dc4127b97
Gim Hangil <gim_hangil@kakao.com>
2022-12-16 01:12:52 +0900

    Simplify BookingView component
    Packed hard-coded components into rendering functions. The main renderer
    iterates through an array named _form items_. During the iteration,
    helper function renders a input component for each element.

--- web/src/views/BookingView/BookingView.js
+++ web/src/views/BookingView/BookingView.js
@@ -3,55 +3,63 @@ import { LabelCard } from 'components';
 import './BookingView.css';
 
 function BookingView() {
+  const form_items = [
+    [
+      { label: '출발', data: ['서울', '부산'], renderer: render_select_input },
+      { label: '도착', data: ['서울', '부산'], renderer: render_select_input },
+    ],
+    [
+      { label: '탑승 날짜', data: 'date' },
+      { label: '출발 가능 시간', data: 'time' },
+      { label: '도착 희망 시간', data: 'time' },
+    ],
+    [
+      { label: '코레일 ID', data: 'text' },
+      { label: '코레일 PW', data: 'password' },
+    ],
+  ];
   return (
     <Box className="BookingView">
-      <Box className="row">
-        <LabelCard label="출발">
-          <Select>
-            <option value="default" disabled={true}>
-              역을 선택해주세요
-            </option>
-            <option>서울</option>
-            <option>동대구</option>
-            <option>부산</option>
-          </Select>
-        </LabelCard>
-        <LabelCard label="도착">
-          <Select>
-            <option value="default" disabled={true}>
-              역을 선택해주세요
-            </option>
-            <option>서울</option>
-            <option>동대구</option>
-            <option>부산</option>
-          </Select>
-        </LabelCard>
-      </Box>
-      <Box className="row">
-        <LabelCard label="탑승 날짜">
-          <Input type="date" />
-        </LabelCard>
-        <LabelCard label="출발 가능 시간">
-          <Input type="time" />
-        </LabelCard>
-        <LabelCard label="도착 희망 시간">
-          <Input type="time" />
-        </LabelCard>
-      </Box>
-      <Box className="row">
-        <LabelCard label="코레일 ID">
-          <Input type="text" />
-        </LabelCard>
-        <LabelCard label="코레일 PW">
-          <Input type="password" />
-        </LabelCard>
-      </Box>
-      <Box className="submit">
-        <Button color="purple" m="xxs">
-          <Text>예약</Text>
-        </Button>
-      </Box>
+      {
+        form_items.map((form_row, idx) =>
+          <Box className="row" key={`row-${idx}`}>
+            {
+              form_row.map(({ label, data, renderer }) => {
+                if (renderer === undefined) {
+                  renderer = render_simple_input;
+                }
+                return (
+                  <LabelCard label={label} key={label}>
+                    { renderer(data) }
+                  </LabelCard>
+                );
+              })
+            }
+          </Box>
+        )
+      }
     </Box>
+  );
+}
+
+function render_select_input(stations) {
+  return (
+    <Select>
+      <option value="default" disabled={true}>
+        역을 선택해주세요
+      </option>
+      {
+        stations.map((station) =>
+          <option key={station}>{station}</option>
+        )
+      }
+    </Select>
+  );
+}
+
+function render_simple_input(input_type) {
+  return (
+    <Input type={input_type} />
   );
 }
 
