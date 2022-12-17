71eb81021eec99797f990dc4d971cef1936cd7d9
Gim Hangil <gim_hangil@kakao.com>
2022-12-17 15:08:10 +0900

    Improve DB setting so that DB URL use env var

--- server/api/database.py
+++ server/api/database.py
@@ -1,14 +1,19 @@
 """SQLAlchemy setup
 """
+from dotenv import load_dotenv
+from os import environ
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite3"
+load_dotenv()
+
+
+DATABASE_URL = environ["DATABASE_URL"]
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
