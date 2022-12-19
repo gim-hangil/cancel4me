8bb6ce3616858f3bc0a92ea00c15a2cfc3062e5a
GIM HANGIL <gim_hangil@kakao.com>
2022-12-19 13:20:39 +0000

    Add specific allowed methods in CORS middleware

--- server/api/main.py
+++ server/api/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
         environ["CORS_ALLOW_URL"],
     ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
