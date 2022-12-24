f8248c4f20bfb1fb11a17bf73366525a74ab100f
Gim Hangil <gim_hangil@kakao.com>
2022-12-25 00:52:30 +0900

    Add spinner component

--- web/src/components/index.js
+++ web/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as LabelCard } from './LabelCard/LabelCard';
 export { default as WaitingCard } from './WaitingCard/WaitingCard';
+export { default as Spinner } from './Spinner/Spinner';
