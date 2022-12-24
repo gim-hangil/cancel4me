a831cfbb39390e47d181b2295e8aaf3ec3d0f38c
Gim Hangil <gim_hangil@kakao.com>
2022-12-25 00:53:07 +0900

    Add spinner in WaitingView
    Shows spinner until API call gets ticket data

--- web/src/views/WaitingView/WaitingView.js
+++ web/src/views/WaitingView/WaitingView.js
@@ -1,14 +1,16 @@
 import { Box } from 'dracula-ui';
-import { WaitingCard } from 'components';
+import { WaitingCard, Spinner } from 'components';
 import { useEffect, useState } from 'react';
 import './WaitingView.css';
 
 function WaitingView() {
+  const [ isLoading, setLoading ] = useState(true);
   const [ tickets, setTickets ] = useState([]);
   useEffect(() => {
     const cached = window.sessionStorage.getItem('tickets');
     if (cached !== null) {
       setTickets(JSON.parse(cached));
+      setLoading(false);
       return;
     }
     (async () => {
@@ -17,25 +19,28 @@ function WaitingView() {
       if (json.status === 'success') {
         setTickets(json.data.sort((a, b) => b.id - a.id));
         window.sessionStorage.setItem('tickets', JSON.stringify(json.data));
+        setLoading(false);
       }
     })();
   }, []);
   return (
     <Box className="WaitingView">
-      {
-        tickets.map((ticket) =>
-          <WaitingCard
-            key={ticket.id}
-            departure_station={ticket.departure_station}
-            arrival_station={ticket.arrival_station}
-            date={ticket.date}
-            departure_base={ticket.departure_base}
-            arrival_limit={ticket.arrival_limit}
-            reserved={ticket.reserved}
-          />
-        )
-      }
+      { isLoading ? <Spinner /> : render_ticket_list(tickets) }
     </Box>
+  );
+}
+
+function render_ticket_list(tickets) {
+  return tickets.map((ticket) =>
+    <WaitingCard
+      key={ticket.id}
+      departure_station={ticket.departure_station}
+      arrival_station={ticket.arrival_station}
+      date={ticket.date}
+      departure_base={ticket.departure_base}
+      arrival_limit={ticket.arrival_limit}
+      reserved={ticket.reserved}
+    />
   );
 }
 
