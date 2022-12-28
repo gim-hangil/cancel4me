c36060fc237107bdf84149ab646c5e17d2c2015e
GIM HANGIL <gim_hangil@kakao.com>
2022-12-28 21:44:21 +0900

    Fix get_tickets function args

--- server/api/crud.py
+++ server/api/crud.py
@@ -1,5 +1,7 @@
 """CRUD functions
 """
+from datetime import datetime
+
 from sqlalchemy.orm import Session
 
 from . import model, schema
@@ -27,26 +29,20 @@ def get_tickets(
     db_session: Session,
     skip: int=0,
     limit: int=100,
-    date=None,
-    dep=None,
-    arr=None,
-    dep_base=None,
-    arr_limit=None,
+    after_now: bool=False,
     reserved: bool=None,
     running: bool=None,
 ) -> list[model.Ticket]:
     """Get ticket reservation records in DB"""
     query = db_session.query(model.Ticket)
-    if date is not None:
-        query = query.filter(model.Ticket.date >= date)
-    if dep is not None:
-        query = query.filter(model.Ticket.departure_station == dep)
-    if arr is not None:
-        query = query.filter(model.Ticket.arrival_station == arr)
-    if dep_base is not None:
-        query = query.filter(model.Ticket.departure_base >= dep_base)
-    if arr_limit is not None:
-        query = query.filter(model.Ticket.arrival_limit <= arr_limit)
+    if after_now:
+        query = query.filter(
+            (model.Ticket.date > datetime.now().date()) |
+            (
+                (model.Ticket.date == datetime.now().date()) &
+                (model.Ticket.departure_base > datetime.now().time())
+            )
+        )
     if reserved is not None:
         query = query.filter(model.Ticket.reserved == reserved)
     if running is not None:
